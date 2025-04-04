159ac753087943936065153c9b1be81a4e4851fd
okorieebube <okorieebube1@gmail.com>
2025-04-04 10:32:58 +0100

    unit test for v2 source

---
+++ test/unit/v2/MoonPieV2Source.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {Test} from "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {MoonPieV2} from "src/v2/MoonPieV2.sol";
+import {UsdcMock} from "../../mocks/UsdcMock.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import {IBridgeAssist} from "src/interfaces/IBridgeAssist.sol";
+import {BaseScript, stdJson, console2} from "script/base.s.sol";
+import {UnsafeUpgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {BridgeAssistTransferUpgradeable} from "../../mocks/BridgeAssistTransferUpgradeable.sol";
+import {EIP712Upgradeable} from "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "forge-std/console2.sol";
+import "../../base/MoonPieSourceBase.sol";
+
+contract MoonPieV2Source is MoonPieSourceBase {
+    function test_initialize() public {
+        MoonPieV2 moonPie = new MoonPieV2(
+            RELAYER_ADDRESS,
+            TREASURY_ADDRESS,
+            MoonPieV2.NETWORKS.ASSET_CHAIN
+        );
+        assertEq(moonPie.RELAYER_ADDRESS(), RELAYER_ADDRESS);
+    }
+
+    function test_bridgeFailsWithZeroAmount() public {
+        MoonPieV2 moonPie = new MoonPieV2(
+            RELAYER_ADDRESS,
+            TREASURY_ADDRESS,
+            MoonPieV2.NETWORKS.BASE
+        );
+        // User inputs zero amount
+        vm.expectRevert(MoonPieV2.InvalidZeroAmount.selector);
+        moonPie.bridge(
+            mockTokenAddress,
+            mockBridgeAddress,
+            0,
+            string.concat("0x", Strings.toHexString(uint160(address(1))))
+        );
+    }
+
+    function test_bridgeInitiatedWithSuccess() public {
+        MoonPieV2 moonPie = new MoonPieV2(
+            RELAYER_ADDRESS,
+            TREASURY_ADDRESS,
+            MoonPieV2.NETWORKS.BASE
+        );
+
+        moonPie.setSupportedNetwork(MoonPieV2.NETWORKS.BASE, "evm.8453");
+        moonPie.setSupportedNetwork(
+            MoonPieV2.NETWORKS.ASSET_CHAIN,
+            "evm.42420"
+        );
+
+        uint256 beforeBalance = usdc.balanceOf(mockBridgeAddress);
+        uint256 treasuryBalanceBefore = usdc.balanceOf(TREASURY_ADDRESS);
+
+        usdc.approve(address(moonPie), usdc.balanceOf(userAddress));
+
+        // Only check that a BridgeInitiated event was emitted, ignore parameter values
+        vm.expectEmit(false, false, false, false);
+        emit MoonPieV2.BridgeInitiated(bytes32(0), "", 0);
+        moonPie.bridge(
+            mockTokenAddress,
+            mockBridgeAddress,
+            10 * 1e6,
+            string.concat("0x", Strings.toHexString(uint160(address(1))))
+        );
+
+        // ensure bridge received funds
+        uint256 afterBalance = usdc.balanceOf(mockBridgeAddress);
+        assertEq(afterBalance - beforeBalance, 99 * 1e5);   //  9.9
+
+        // ensure moonpie treasury got expected fee
+        uint256 treasuryBalanceAfter = usdc.balanceOf(TREASURY_ADDRESS);
+        assertEq(treasuryBalanceAfter - treasuryBalanceBefore, 0.1 * 1e6); // 0.1
+    }
+}
